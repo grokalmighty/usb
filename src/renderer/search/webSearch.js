7d959a00d356abc5a1a19b2b2433a719e2cb4fa9
SH4RD3D <jaramode68duck.com>
2025-11-21 12:20:34 -0500

    Web search class constructor

---
+++ src/renderer/search/webSearch.js
@@ -0,0 +1,9 @@
+class WebSearch {
+    constructor() {
+        this.searchEngines = {
+            google: {name: 'Google', url: 'https://google.com/search?q=' },
+            duckduckgo: {name: 'DuckDuckGo', url: 'https://duckduckgo.com/?q='},
+            github: {name: 'GitHub', url: 'https://github.com/search?q='}
+            };
+        }
+    }
